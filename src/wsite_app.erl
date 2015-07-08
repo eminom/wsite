de39b9abf669e666b18162d7daac5a758a46c223
home <hai.bai.cn@gmail.com>
2015-07-08 18:02:44 +0800

    add / handler wsite

--- src/wsite_app.erl
+++ src/wsite_app.erl
@@ -5,6 +5,14 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+	Dispatcher = cowboy_http:compile(
+		[ {'_', [{"/", wsite_handler,[]}]} ]
+	),
+	{ok, _} = cowboy:start_http(
+		my_http_listener, 
+		100, [{port, 8083}],
+		[{env, [{dispatch, Dispatcher}]}]
+	),
 	wsite_sup:start_link().
 
 stop(_State) ->
